b53477b064590b8b7d07b91ca92303900480c4f7
biffotmc <86079175+biffotmc@users.noreply.github.com>
2022-08-16 10:00:12 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Shoutbox Tickets'
+copyright = '2022, Biffot'
+author = 'Biffot'
 
 release = '0.1'
 version = '0.1.0'
